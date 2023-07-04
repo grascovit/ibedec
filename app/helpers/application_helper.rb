e62ec848db6055aeed088112a280cb1e39c5cffc
Gabriel Rascovit <rascovit@gmail.com>
2023-07-04 11:04:53 -0300

    add canonical url to the layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def canonical_url
+    url = url_for(only_path: false)
+    url += '?' if request.query_parameters.any?
+
+    request.query_parameters.each do |key, value|
+      url += "#{key}=#{value}"
+    end
+
+    url
+  end
 end
